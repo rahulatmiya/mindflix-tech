657f9115353064d85b5efa7370e3942db149c1c7
ASHUTOSH YADAV YADAV <admin@mindsettersg.onmicrosoft.com>
2026-06-02 15:49:56 +0530

    Update vite file  build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/mindflix-tech/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
